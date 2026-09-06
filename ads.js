badc942786645c4c3dede518f8f4859635e91d15
Igor <63743080+ipavlin98@users.noreply.github.com>
2026-09-06 07:10:53 +0300

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,38 @@
+(function () {
+    function initLampaHook() {
+        if (window.Lampa && Lampa.Player && Lampa.Player.listener) {
+            var listener = Lampa.Player.listener;
+            var originalSend = listener.send;
+            var restore;
+
+            listener.send = function (type, event) {
+                if (restore && (type === 'create' || type === 'start' || type === 'external' || type === 'destroy')) {
+                    restore();
+                    restore = null;
+                }
+
+                var result = originalSend.apply(this, arguments);
+
+                if (type === 'create' && event && event.data) {
+                    var data = event.data;
+                    var hadIptv = Object.prototype.hasOwnProperty.call(data, 'iptv');
+                    var iptv = data.iptv;
+
+                    restore = function () {
+                        if (hadIptv) data.iptv = iptv;
+                        else delete data.iptv;
+                    };
+
+                    data.iptv = true;
+                    delete data.vast_url;
+                    delete data.vast_msg;
+                }
+
+                return result;
+            };
+        } else {
+            setTimeout(initLampaHook, 500);
+        }
+    }
+    initLampaHook();
+})();
